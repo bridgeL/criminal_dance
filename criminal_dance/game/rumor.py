39ced042d054cb4b3f46296414297f7bb944af7a
Su <wxlxy316@163.com>
2023-01-19 16:10:54 +0800

    增加 谣言

--- criminal_dance/game/rumor.py
+++ criminal_dance/game/rumor.py
@@ -1,4 +1,5 @@
 '''谣言'''
+from random import choice
 from ..cat import cat
 from ..model import Game
 from .utils import turn_next, check_player, check_card, play_card, check_first
@@ -23,6 +24,16 @@ async def rumor():
 
         # 所有有手牌的人，寻找有手牌的下家，同时、同步抽取一张牌
 
-        #
+        ps = [p for p in game.players if p.cards]
+
+        cs = []
+        for p in ps:
+            c = choice(p.cards)
+            p.cards.remove(c)
+            cs.append(c)
+
+        cs = [*cs[1:], cs[0]]
+        for c, p in zip(cs, ps):
+            p.cards.append(c)
 
         await turn_next()
