ed18731c1c80fc1d554d39abbff1dd6a20f53ad1
Su <wxlxy316@163.com>
2023-01-20 12:05:10 +0800

    优化犯人表现

--- criminal_dance/game/criminal.py
+++ criminal_dance/game/criminal.py
@@ -23,11 +23,13 @@ async def criminal():
             return
 
         await player.play_card(card)
+        await game.send(f"{R.犯人}是 {player.index_name}！")
 
         # 被警部抓到了
         if game.police.target_id == player.id:
             p2 = game.get_player(game.police.owner_id)
             p2.is_good = True
+            await game.send(f"但是{R.犯人}被 {p2.index_name} 布置的{R.警部}抓到了！")
             return await game.end(True)
 
         await game.end(False)
